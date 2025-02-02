1996685e7d85258806e152e752280f4c2d62a31a
Aaraiz-OP <maxhacker295@gmail.com>
2025-02-02 18:54:24 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+def Minecraft
+print ("Hi m name is Aaraiz and i play Minecraft 1.20.6 ")
+Minecraft ()
+#Method2
+def add (number1,number2)
+result = number1+number2
+print ("The answer is", result)
+def min (number1,number2)
+result = number1-number2
+print ("The answer is", result)
